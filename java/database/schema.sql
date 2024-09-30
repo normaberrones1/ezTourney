db238e52a74c7c3784ff1fcc5a952adc79533429
AlHeather <hg06232023@gmail.com>
2024-09-30 12:40:13 -0400

    Game data update for database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -3,6 +3,8 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS teams;
 DROP TABLE IF EXISTS team_users;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS game_category;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -12,22 +14,38 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE game_category(
+    category_id SERIAL,
+    category_name varchar(50) NOT NULL,
+    CONSTRAINT PK_category PRIMARY KEY (category_id)
+);
+
+CREATE TABLE games (
+    game_id SERIAL,
+    game_name varchar(50) NOT NULL,
+    category_id int NOT NULL,
+    CONSTRAINT PK_games PRIMARY KEY (game_id),
+    CONSTRAINT FK_category FOREIGN KEY (category_id) REFERENCES game_category(category_id)
+);
+
 CREATE TABLE teams (
     team_id SERIAL,
     team_name varchar(50) NOT NULL,
     captain_id int NOT NULL,
-    sport varchar(50) NOT NULL,
+    game_id int NOT NULL,
     isAccepting boolean NOT NULL,
-    CONSTRAINT pk_team PRIMARY KEY (team_id),
-    CONSTRAINT fk_captain FOREIGN KEY (captain_id) REFERENCES users(user_id)
+    max_players int NOT NULL,
+    CONSTRAINT PK_team PRIMARY KEY (team_id),
+    CONSTRAINT FK_captain FOREIGN KEY (captain_id) REFERENCES users(user_id),
+    CONSTRAINT FK_game FOREIGN KEY (game_id) REFERENCES games(game_id)
 );
 
 CREATE TABLE team_users (
     user_id int NOT NULL,
     team_id int NOT NULL,
-    CONSTRAINT pk_team_users PRIMARY KEY (user_id, team_id),
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
-    CONSTRAINT fk_team_id FOREIGN KEY (team_id) REFERENCES teams(team_id)
+    CONSTRAINT PK_team_users PRIMARY KEY (user_id, team_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT FK_team_id FOREIGN KEY (team_id) REFERENCES teams(team_id)
 );
 
 
