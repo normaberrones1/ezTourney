97ccf2974f280cfc11886ba0e49e30a0d25cb6f8
AlHeather <hg06232023@gmail.com>
2024-09-30 17:51:18 -0400

    views to give routes

---
+++ vue/src/components/TeamCard.vue
@@ -0,0 +1,43 @@
+<!-- 
+Data for an individual team.
+Team captain and team name will be visible to public
+if user isnt authenticated don't show list of team members... how to do that though.
+probably put a isAuthenticated on the "getMembersByTeam method"
+if we even choose to do that.
+-->
+
+<template>
+    <h2>{{ team.teamName }}</h2>
+    <h4>{{ teamCaptain.username }}</h4>
+</template>
+
+<script>
+import MemberService from '../services/MemberService.js';
+
+export default {
+    
+
+    data(){
+        return {
+            teamCaptain: {
+                username: "",
+                userId: ""
+            },
+
+            users: []
+        }
+    },
+
+    props: ["team"],
+
+    created(){
+        MemberService.getCaptainById(team.captainId).then((response) => {
+            teamCaptain = response.data;
+        });
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/src/components/TeamList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-for="team in teams" :key="team.teamId">
+        <teamCard v-bind:team="team"/>
+    </div>
+</template>
+
+<script>
+import TeamCard from './TeamCard.vue';
+import TeamService from '../services/TeamService.js';
+
+
+export default {
+    data() {
+        return {
+            teams: [],
+        }
+    },
+
+    components: [TeamCard],
+
+    created(){
+
+    }
+}
+
+</script>
+
+<style>
+
+</style>
