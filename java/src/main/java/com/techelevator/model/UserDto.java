9bc3e04dcba519ce717d0daca69875c24312a3c1
AlHeather <hg06232023@gmail.com>
2024-09-30 15:34:50 -0400

    Member dao, jdbc member dao, and member controller implimented

---
+++ java/src/main/java/com/techelevator/model/UserDto.java
@@ -0,0 +1,23 @@
+package com.techelevator.model;
+
+public class UserDto {
+
+    private String username;
+    private int userId;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
