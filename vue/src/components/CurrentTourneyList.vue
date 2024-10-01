0edf94f7d5f7243e764d86ae93932daeaf18c87b
AlHeather <hg06232023@gmail.com>
2024-10-01 19:10:43 -0400

    done for today

--- vue/src/components/CurrentTourneyList.vue
+++ vue/src/components/CurrentTourneyList.vue
@@ -2,9 +2,12 @@
     <div class="tournament"></div>
     <h1>this page will display the current tournaments in the system</h1>
     <h2>clicking a tourney will give the actual brackets and the info</h2>
-    <router-link>
-    <TournamentCard />
-    </router-link>
+    <div>
+        <router-link v-for="tourney in tournaments" :key="tourney.tourneyId"
+            v-bind:to="{ name: 'tourneyDetails', params: { id: tourney.tourneyId } }">
+            <TournamentCard v-bind:tourney="tourney" />
+        </router-link>
+    </div>
 </template>
 
 <script>
@@ -20,7 +23,7 @@ export default {
 
     components: { TournamentCard },
 
-    created(){
+    created() {
         TourneyService.getCurrentTournaments().then((response) => {
             this.tournaments = response.data;
         })
