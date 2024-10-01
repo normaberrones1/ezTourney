ddde7dfd0cfd90892e9014714694906c3bcc615f
AlHeather <hg06232023@gmail.com>
2024-10-01 11:18:57 -0400

    team detiails stuff

--- vue/src/components/TeamDetails.vue
+++ vue/src/components/TeamDetails.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <h1>{{team.teamName}}</h1>
+        <h1>{{ team.teamName }}</h1>
+        <h3>{{ captain.username }}</h3>
+    </div>
+    <div>
+
     </div>
 </template>
 
 <script>
- import TeamService from '../services/TeamService.js'
+import TeamService from '../services/TeamService.js'
+import MemberService from '../services/MemberService.js';
 
 export default {
-   data(){
-        return{
+    data() {
+        return {
             teamId: -1,
             team: {},
+            captain: {},
+            members: [],
         }
     },
 
@@ -20,11 +27,15 @@ export default {
         this.teamId = this.$route.params.teamId;
         TeamService.getTeamById(this.teamId).then((response) => {
             this.team = response.data;
+            MemberService.getMemberById(this.team.captainId).then((response) => {
+                this.captain = response.data;
+            });
+        });
+        MemberService.getMembersByTeam(this.teamId).then((response) => {
+            
         })
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
