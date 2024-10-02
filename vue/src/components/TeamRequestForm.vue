3951e51d1e5851f82b74cd87a2e8edddaa5e7683
Pheebe Deshapogu <pheebe.deshapogu@gmail.com>
2024-10-02 11:26:55 -0400

    removed some code

--- vue/src/components/TeamRequestForm.vue
+++ vue/src/components/TeamRequestForm.vue
@@ -1,25 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div>
-        <h1>Team Request Form</h1>
-        <form @submit.prevent="submitForm">
-            <div class="form-group">
-                <label for="teamName">Team Name</label>
-                <input type="text" id="teamName" v-model="teamName" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="teamDescription">Team Description</label>
-                <textarea id="teamDescription" v-model="teamDescription" class="form-control"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="teamMembers">Team Members</label>
-                <input type="text" id="teamMembers" v-model="teamMembers" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
-</template>
-=======
   <div>
     <h1>Team Request Form</h1>
     <p>Request to join {{this.teamName}}!</p>
@@ -78,4 +57,4 @@ export default {
 <style>
 
 </style>
->>>>>>> 3e8c56ac3fa65101a66bc6b99b7524dc1fe3655e
+
